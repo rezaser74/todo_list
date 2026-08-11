94fec31906352bfdfbec99a0f2815f875131bf10
rezaser <rezaser3774@gmail.com>
2026-08-11 14:34:05 +0330

    init: add business logics model into Todo.php

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -9,4 +9,13 @@ class Todo extends Model
 {
     /** @use HasFactory<\Database\Factories\TodoFactory> */
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'is_completed',
+    ];
+    // is completed task getter
+    // business logic for updating a todo
+    public function markAsComplete():bool{
+       return  $this->update(['is_completed' => true]);
+    }
 }
